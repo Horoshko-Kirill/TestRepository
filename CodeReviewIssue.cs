40773466b9551d25695e7e8d33bdfb4b601051a2
Horoshko_K <140325016+Horoshko-Kirill@users.noreply.github.com>
2026-04-01 02:34:51 +0300

    Update CodeReviewIssue.cs

--- CodeReviewIssue.cs
+++ CodeReviewIssue.cs
@@ -2,7 +2,7 @@
  
 public sealed class CodeReviewIssue 
 {
-    public string Severity { get; init; } = "info"; // info|warning|error      
+    public string Severity { get; init; } = "info"; // info|warning|error          
     public string Title { get; init; } = default!;
     public string Details { get; init; } = default!;    
 }
