4c3cb0ce65ed7a5d0dcc4ec5eb861f17ddc104cd
kiril <khorosko15@gmail.com>
2026-04-14 15:27:01 +0300

    test

--- CodeReviewIssue.cs
+++ CodeReviewIssue.cs
@@ -1,5 +1,5 @@
 ﻿namespace AiMultiAgent.Core.Agents.CodeReview;
-
+   
 public sealed class CodeReviewIssue   
 {
     public string Severity { get; init; } = "info"; // info|warning|error
--- CodeReviewResult.cs
+++ CodeReviewResult.cs
@@ -1,5 +1,5 @@
 ﻿ namespace AiMultiAgent.Core.Agents.CodeReview;
-
+   
 public sealed class CodeReviewResult   
 {
     public string Summary { get; init; } = default!;
